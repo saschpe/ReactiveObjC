c45246aaea1b6e5b11e2e102f9384514fe4bba30
Igor Makarov <igor.makarov@moovitapp.com>
2020-07-26 09:56:26 +0300

    checkout 3.1.0

---
+++ Sources/ReactiveObjC/UISlider+RACSignalSupport.m
@@ -0,0 +1,22 @@
+#if (TARGET_OS_IOS)
+//
+//  UISlider+RACSignalSupport.m
+//  ReactiveObjC
+//
+//  Created by Uri Baghin on 20/07/2013.
+//  Copyright (c) 2013 GitHub, Inc. All rights reserved.
+//
+
+#import "UISlider+RACSignalSupport.h"
+#import "EXTKeyPathCoding.h"
+#import "UIControl+RACSignalSupportPrivate.h"
+
+@implementation UISlider (RACSignalSupport)
+
+- (RACChannelTerminal *)rac_newValueChannelWithNilValue:(NSNumber *)nilValue {
+	return [self rac_channelForControlEvents:UIControlEventValueChanged key:@keypath(self.value) nilValue:nilValue];
+}
+
+@end
+
+#endif
---
+++ Sources/ReactiveObjC/include/RACSignalProvider.h
@@ -0,0 +1,6 @@
+#define RACSIGNAL_NEXT_ENABLED() 0
+#define RACSIGNAL_NEXT(a,b,c) do {} while(0)
+#define RACSIGNAL_ERROR_ENABLED() 0
+#define RACSIGNAL_ERROR(a,b,c) do {} while(0)
+#define RACSIGNAL_COMPLETED_ENABLED() 0
+#define RACSIGNAL_COMPLETED(a,b) do {} while(0)
