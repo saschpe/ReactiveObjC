126a930597c3dcc998a502bd752095027e04b985
Sascha Peilicke <sascha@peilicke.de>
2024-05-13 17:58:04 +0200

    Conform to SPM include header layout

---
+++ Tests/Unit/ImportTest.m
@@ -0,0 +1,14 @@
+#import <XCTest/XCTest.h>
+#import <ReactiveObjC/ReactiveObjC.h>
+
+@implementation XCTestCase
+
+- (void)setUp {
+  [super setUp];
+}
+
+- (void)tearDown {
+  [super tearDown];
+}
+
+@end
